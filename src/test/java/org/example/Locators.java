7f4658f78be86b3769751a75eb0329e2d8b81dca
Md_Ashikujjaman <ashik35uap@gmail.com>
2025-05-24 23:34:44 +0600

    Use Implicit Wait

--- src/test/java/org/example/Locators.java
+++ src/test/java/org/example/Locators.java
@@ -3,52 +3,44 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class Locators extends DriverSetup{
 
     @Test
     public void testLocator() throws InterruptedException {
-        /*driver.get("https:www.google.com");
+        driver.get("https:www.google.com");
         WebElement searchArea = driver.findElement(By.id("APjFqb"));
         searchArea.sendKeys("Dhaka");
-        Thread.sleep(3000);
-
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //Implicit wait, Handel driver error like 'NoSuchElementException'.
+//        Thread.sleep(3000);
         driver.findElement(By.className("gNO89b")).click();
 
-        driver.switchTo().newWindow(WindowType.TAB);*/
+        driver.switchTo().newWindow(WindowType.TAB);  //open new tab
         driver.get("https://www.google.com/maps/");
         driver.manage().window().fullscreen();
-//        driver.findElement(By.cssSelector("button[aria-label='মেনু']")).click();
-//        Thread.sleep(2000);
-//        driver.findElement(By.cssSelector("button.aAaxGf.T2ozWe")).click();
-//        Thread.sleep(2000);
-//        driver.findElement(By.partialLinkText("English")).click();
-//        Thread.sleep(2000);
-//        driver.findElement(By.cssSelector("a[href*='google.com/maps'][href*='hl=en']")).click();
-        /*driver.findElement(By.className("wR3cXd")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.className("aAaxGf")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.partialLinkText("English")).click();*/
-//        Thread.sleep(2000);
+
+
 
         WebElement searchInMap = driver.findElement(By.className("xiQnY"));
         searchInMap.sendKeys("Farmgate Metro Rail Station");
-        Thread.sleep(2000);
+//        Thread.sleep(2000);
         driver.findElement(By.className("mL3xi")).click();
-        Thread.sleep(2000);
+//        Thread.sleep(2000);
 
         driver.findElement(By.className("g88MCb")).click();
-        Thread.sleep(2000);
+//        Thread.sleep(2000);
 
         WebElement startPoint = driver.findElement(By.className("tactile-searchbox-input"));
         driver.findElement(By.className("tactile-searchbox-input")).clear();
         startPoint.sendKeys("Manik Mian Avenue West Bus Stop");
-        Thread.sleep(2000);
+//        Thread.sleep(2000);
         driver.findElement(By.cssSelector("button[aria-label = 'খুঁজুন']")).click();
-        Thread.sleep(2000);
+//        Thread.sleep(2000);
 //        driver.findElement(By.className("m6Uuef")).click();
         driver.findElement(By.cssSelector("button[data-tooltip = 'হাঁটা']")).click();
-        Thread.sleep(10000);
+//        Thread.sleep(10000);
 //walking distance
         String byRoad = driver.findElement(By.id("section-directions-trip-title-0")).getText();
         System.out.println("Road : " + byRoad);
