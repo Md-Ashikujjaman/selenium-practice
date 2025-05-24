7821c9731c83cb19846aab8d781ba17dc6895d94
Md_Ashikujjaman <ashik35uap@gmail.com>
2025-05-24 23:39:38 +0600

    Practice dropdown select

--- src/test/java/org/example/HandelDropdownAlertFrame.java
+++ src/test/java/org/example/HandelDropdownAlertFrame.java
@@ -1,4 +1,22 @@
 package org.example;
 
-public class HandelDropdownAlertFrame {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class HandelDropdownAlertFrame extends DriverSetup{
+
+    @Test
+    public void interactWithDropdown() throws InterruptedException{
+
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+        WebElement dropdownMenu = driver.findElement(By.id("dropdown"));
+        Select select = new Select(dropdownMenu);
+        dropdownMenu.click();
+
+        select.selectByIndex(2);
+
+        Thread.sleep(3000);
+    }
 }
